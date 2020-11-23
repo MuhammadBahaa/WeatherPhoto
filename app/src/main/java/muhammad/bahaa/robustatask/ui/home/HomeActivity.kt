90f4859af1236156312311a0945a2ae6c1b956ad
Muhammad Bahaa <muhammadbahaa75@gmail.com>
2020-11-23 10:01:30 +0200

    [ADD] Add Home Screen

--- app/src/main/java/muhammad/bahaa/robustatask/ui/home/HomeActivity.kt
+++ app/src/main/java/muhammad/bahaa/robustatask/ui/home/HomeActivity.kt
@@ -1,18 +1,52 @@
 package muhammad.bahaa.robustatask.ui.home
 
 
+import android.content.pm.PackageManager
+import androidx.lifecycle.Observer
 import muhammad.bahaa.robustatask.R
 import muhammad.bahaa.robustatask.databinding.ActivityHomeBinding
 import muhammad.bahaa.robustatask.ui.base.BaseActivity
+import muhammad.bahaa.robustatask.utils.REQUEST_CAMERA
+import muhammad.bahaa.robustatask.utils.fileToStoreCameraImg
+import muhammad.bahaa.robustatask.utils.openCamera
 
-class HomeActivity : BaseActivity<HomeViewModel, ActivityHomeBinding>(){
+class HomeActivity : BaseActivity<HomeViewModel, ActivityHomeBinding>() {
 
     override val rootView: Int
         get() = R.layout.activity_home
     override val vMType: Class<HomeViewModel> = HomeViewModel::class.java
 
     override fun attachViewModelToView() {
+        setObservers()
+        initializeDataBindingVariables()
+    }
+
+    private fun initializeDataBindingVariables() {
         viewDataBinding.homeViewModel = viewModel
         viewDataBinding.lifecycleOwner = this
+    }
+
+    private fun setObservers() {
+
+        val openCameraObserver = Observer<Unit> {
+            openCamera(fileToStoreCameraImg, REQUEST_CAMERA)
+        }
+
+        viewModel.takePhotoEvent.observe(this, openCameraObserver)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CAMERA -> {
+                if (grantResults.isNotEmpty() || grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    this.openCamera(fileToStoreCameraImg)
+
+                }
+            }
+        }
     }
 }
--- app/src/main/java/muhammad/bahaa/robustatask/ui/home/HomeViewModel.kt
+++ app/src/main/java/muhammad/bahaa/robustatask/ui/home/HomeViewModel.kt
@@ -1,7 +1,16 @@
 package muhammad.bahaa.robustatask.ui.home
 
+import androidx.lifecycle.LiveData
 import muhammad.bahaa.robustatask.ui.base.BaseViewModel
+import muhammad.bahaa.robustatask.utils.SingleLiveEvent
 
 class HomeViewModel : BaseViewModel() {
 
+    private val _takePhotoEvent = SingleLiveEvent<Unit>()
+    val takePhotoEvent: LiveData<Unit>
+        get() = _takePhotoEvent
+
+    fun onTakePhotoButtonClicked(){
+        _takePhotoEvent.call()
+    }
 }
